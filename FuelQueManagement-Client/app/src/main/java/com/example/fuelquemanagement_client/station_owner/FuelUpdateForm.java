01001917bebc49e85cfe4b062b098d200e8f7920
MaduwanthaNND <it19026480@my.sliit.lk>
2022-10-19 00:00:35 +0530

    fuel_update_api_call

--- FuelQueManagement-Client/app/src/main/java/com/example/fuelquemanagement_client/station_owner/FuelUpdateForm.java
+++ FuelQueManagement-Client/app/src/main/java/com/example/fuelquemanagement_client/station_owner/FuelUpdateForm.java
@@ -6,9 +6,11 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -16,15 +18,36 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.fuelquemanagement_client.R;
 
-import java.util.Calendar;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class FuelUpdateForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class FuelUpdateForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
     private TextView tv_arrival_date, tv_arrival_time;
     private int hour, min;
     private EditText et_fuel_amount;
     private Spinner sp_fuel_type;
+    private Button btn_update;
+    private RequestQueue requestQueue;
+    private String selectedType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +61,8 @@ public class FuelUpdateForm extends AppCompatActivity implements AdapterView.OnI
         et_fuel_amount = findViewById(R.id.editTextFuelAmount);
         tv_arrival_date = findViewById(R.id.tv_ArrivalDate);
         tv_arrival_time = findViewById(R.id.tv_ArrivalTime);
+        btn_update = findViewById(R.id.btn_update);
+        btn_update.setOnClickListener(this);
         sp_fuel_type = (Spinner) findViewById(R.id.sp_fuel_type);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.fuel_types, android.R.layout.simple_spinner_item);
@@ -93,12 +118,73 @@ public class FuelUpdateForm extends AppCompatActivity implements AdapterView.OnI
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
-        String selectedItem = (String) adapterView.getItemAtPosition(pos);
-        Toast.makeText(this, selectedItem, Toast.LENGTH_SHORT).show();
+        selectedType = (String) adapterView.getItemAtPosition(pos).toString();
+        Toast.makeText(this, selectedType, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_update) {
+            postFuelUpdate();
+        }
+    }
+
+    private void postFuelUpdate() {
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            String URL = "http://192.168.1.71:5000/Fuel";
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("type", selectedType);
+            jsonObject.put("amount", et_fuel_amount.getText().toString());
+            jsonObject.put("date", tv_arrival_date.getText().toString());
+            jsonObject.put("time", tv_arrival_time.getText().toString());
+            jsonObject.put("stationsId", "634daba7601fef8b9474a684");
+            final String mRequestBody = jsonObject.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("LOG_VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
