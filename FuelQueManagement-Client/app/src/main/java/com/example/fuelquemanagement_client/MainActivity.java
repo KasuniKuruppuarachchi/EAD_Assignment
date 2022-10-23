2fa6a632de72a9b93c211044cf9bc396ffd9d078
MaduwanthaNND <it19026480@my.sliit.lk>
2022-10-24 00:36:13 +0530

    landing_page_updated

--- FuelQueManagement-Client/app/src/main/java/com/example/fuelquemanagement_client/MainActivity.java
+++ FuelQueManagement-Client/app/src/main/java/com/example/fuelquemanagement_client/MainActivity.java
@@ -9,59 +9,26 @@ import android.widget.Button;
 
 import com.example.fuelquemanagement_client.constants.Constants;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private Button btnStationOwner, btnVehicleOwner, btnLogin;
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-//            case R.id.btn_shed_owner:
-//                System.out.println("Clicked123");
-//                Intent i = new Intent(MainActivity.this, StationOwnerDashboard.class);
-//                startActivity(i);
-//                break;
-//            case R.id.btn_vehicle_owner:
-//                System.out.println("Clicked123");
-//                break;
-//            default:
-//                break;
-        }
-
-    }
+/**
+ * The Main class facilitates the launching interface of the application
+ */
+public class MainActivity extends AppCompatActivity {
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btn_started);
 
-        btnStationOwner = findViewById(R.id.btn_shed_owner);
-        btnVehicleOwner = findViewById(R.id.btn_vehicle_owner);
-        btnLogin = findViewById(R.id.btn_login);
-
-        btnStationOwner.setOnClickListener(new View.OnClickListener() {
+        // Setting the Get Started button navigation
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, Registration.class);
-                i.putExtra(Constants.ROLE, Constants.STATION);
-                startActivity(i);
-            }
-        });
-
-        btnVehicleOwner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, Registration.class);
-                i.putExtra(Constants.ROLE, Constants.VEHICLE);
-                startActivity(i);
-            }
-        });
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, LoginScreen.class);
-                startActivity(i);
+                Intent intent = new Intent(MainActivity.this, LoginScreen.class);
+                startActivity(intent);
             }
         });
     }
